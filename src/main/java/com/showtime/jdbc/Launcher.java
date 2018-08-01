b8c08ab083dd11708dbab11ca08a431de47677cc
yaoweihao <yaoweihao@jd.com>
2018-08-01 09:43:22 +0800

    sharding-jdbc-sample初始化

---
+++ src/main/java/com/showtime/jdbc/Launcher.java
@@ -0,0 +1,13 @@
+package com.showtime.jdbc;
+
+/**
+ * Created with IntelliJ IDEA
+ *
+ * @description: 启动器
+ * @author: yaoweihao
+ * @date: 2018/8/1
+ * @time: 9:39
+ * @modified by:
+ */
+public class Launcher {
+}
