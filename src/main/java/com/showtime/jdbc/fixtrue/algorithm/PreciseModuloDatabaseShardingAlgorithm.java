23c1c73f7cfd68c7b1fe559289857ad245e38277
yaoweihao <yaoweihao@jd.com>
2018-08-02 17:55:12 +0800

    完成自测

--- src/main/java/com/showtime/jdbc/fixtrue/algorithm/PreciseModuloDatabaseShardingAlgorithm.java
+++ src/main/java/com/showtime/jdbc/fixtrue/algorithm/PreciseModuloDatabaseShardingAlgorithm.java
@@ -22,12 +22,12 @@ import io.shardingsphere.core.api.algorithm.sharding.standard.PreciseShardingAlg
 
 import java.util.Collection;
 
-public final class PreciseModuloDatabaseShardingAlgorithm implements PreciseShardingAlgorithm<Integer> {
+public final class PreciseModuloDatabaseShardingAlgorithm implements PreciseShardingAlgorithm<String> {
     
     @Override
-    public String doSharding(final Collection<String> availableTargetNames, final PreciseShardingValue<Integer> shardingValue) {
+    public String doSharding(final Collection<String> availableTargetNames, final PreciseShardingValue<String> shardingValue) {
         for (String each : availableTargetNames) {
-            if (each.endsWith(shardingValue.getValue() % 2 + "")) {
+            if (each.endsWith(shardingValue.hashCode() % 2 + "")) {
                 return each;
             }
         }
